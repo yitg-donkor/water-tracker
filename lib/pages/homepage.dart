71d719d565e064f85b2974fee53da44353b39704
Donkor <ahofecarter@gmail.com>
2025-04-16 08:02:55 +0000

    running it back

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -8,26 +8,77 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  // Style variables
+  late TextStyle large;
+  late TextStyle small;
+  late Color backgroundColor = Color.fromRGBO(244, 248, 251, 1);
+  late Color iconColor;
+  late double screenheight;
+  late double screenwidth;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Initialize text styles that need BuildContext
+    small = Theme.of(context).textTheme.bodySmall!;
+    large = Theme.of(context).textTheme.bodyLarge!;
+    screenheight = MediaQuery.of(context).size.height;
+    screenwidth = MediaQuery.of(context).size.width;
+
     return Container(
-      color: Colors.white,
+      color: backgroundColor,
       child: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    "Good Morning",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                      decoration: TextDecoration.none,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Good Morning", style: small),
+                      Text('Benjamin Donkor', style: large),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Icon(
+                        Icons.notifications,
+                        size: 25,
+                        color: Colors.lightBlue,
+                      ),
                     ),
                   ),
                 ],
+              ),
+              SizedBox(height: screenheight * 0.02),
+              Card(
+                child: Container(
+                  color: Colors.white,
+                  height: screenheight * 0.2,
+                  width: screenwidth * 0.9,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('11:00 Am', style: large),
+                        Text('200ml water(2 Glass)', style: small),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ],
           ),
